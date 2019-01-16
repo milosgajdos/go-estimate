ef3bc0c48b4ff238a4beeb507d39fa9ae060cbd7
Milos Gajdos <milosgajdos83@gmail.com>
2019-01-16 22:15:15 +0000

    Smoothing impementation
    First attempt to implement RTS Smoothing is done.

--- smooth/rts/rts.go
+++ smooth/rts/rts.go
@@ -1,7 +1,10 @@
 package rts
 
 import (
+	"fmt"
+
 	filter "github.com/milosgajdos83/go-filter"
+	"github.com/milosgajdos83/go-filter/estimate"
 	"gonum.org/v1/gonum/mat"
 )
 
@@ -27,5 +30,62 @@ func New(init filter.InitCond) (*RTS, error) {
 // It uses updated estimates u, predicted estimates p and dynamics matrices m and returns smoothed estimates.
 // It returns error if either the sizes of p and u differ or the smoothed estimates failed to be calculated.
 func (s *RTS) Smooth(p []filter.Estimate, u []filter.Estimate, m []mat.Matrix) ([]filter.Estimate, error) {
-	return nil, nil
+	if len(p) != len(u) || len(p) != len(m) {
+		return nil, fmt.Errorf("Invalid filter history size")
+	}
+
+	sx := make([]filter.Estimate, len(p)-1)
+
+	// P-1 inverse
+	pinv := &mat.Dense{}
+	// intermediate smoothing matrix
+	c := &mat.Dense{}
+	// smoothed state
+	x := &mat.Dense{}
+
+	// smoothed covariance
+	cov := &mat.Dense{}
+	pk := &mat.Dense{}
+
+	for i := len(p) - 1; i > 0; i-- {
+		// Pk*Fk'
+		c.Mul(u[i-1].Cov(), m[i-1].T())
+		// invert predicted P_k+1 covariance
+		if err := pinv.Inverse(p[i].Cov()); err != nil {
+			return nil, err
+		}
+		// Pk*Fk'*P(k+1)_-1
+		c.Mul(c, pinv)
+
+		// smooth the state
+		x.Sub(u[i].Val(), p[i].Val())
+		// c*x
+		x.Mul(c, x)
+		// xk + Ck*x_sub
+		x.Add(x, p[i-i].Val())
+
+		// smooth covariance
+		cov.Sub(u[i].Cov(), p[i].Cov())
+		pk.Mul(c, cov)
+		pk.Mul(pk, pk.T())
+		pk.Add(pk, u[i-1].Cov())
+
+		r, _ := cov.Dims()
+		pSmooth := mat.NewSymDense(r, nil)
+		// update KF covariance matrix
+		for i := 0; i < r; i++ {
+			for j := i; j < r; j++ {
+				pSmooth.SetSym(i, j, pk.At(i, j))
+			}
+		}
+
+		est, err := estimate.NewBaseWithCov(x.ColView(0), pSmooth)
+		if err != nil {
+			return nil, err
+		}
+
+		sx[i-1] = est
+	}
+
+	return sx, nil
 }
--- smooth/rts/rts_test.go
+++ smooth/rts/rts_test.go
@@ -4,6 +4,8 @@ import (
 	"os"
 	"testing"
 
+	filter "github.com/milosgajdos83/go-filter"
+	"github.com/milosgajdos83/go-filter/estimate"
 	"github.com/milosgajdos83/go-filter/sim"
 	"github.com/stretchr/testify/assert"
 	"gonum.org/v1/gonum/mat"
@@ -11,6 +13,9 @@ import (
 
 var (
 	ic *sim.InitCond
+	ex []filter.Estimate
+	mx []mat.Matrix
+	n  int
 )
 
 func setup() {
@@ -18,6 +23,16 @@ func setup() {
 	initState := mat.NewVecDense(2, []float64{1.0, 3.0})
 	initCov := mat.NewSymDense(2, []float64{0.25, 0, 0, 0.25})
 	ic = sim.NewInitCond(initState, initCov)
+
+	n = 2
+	// generate some estimates
+	for i := 0; i < 5; i++ {
+		e, _ := estimate.NewBaseWithCov(
+			mat.NewVecDense(n, []float64{1.0, 3.0}),
+			mat.NewSymDense(n, []float64{0.25, 0, 0, 0.25}))
+		mx = append(mx, mat.NewDense(2, 2, []float64{1.0, 1.0, 1.0, 1.0}))
+		ex = append(ex, e)
+	}
 }
 
 func TestMain(m *testing.M) {
@@ -34,5 +49,21 @@ func TestNewRTS(t *testing.T) {
 
 	s, err := New(ic)
 	assert.NotNil(s)
+	assert.NoError(err)
+}
+
+func TestRTSSmooth(t *testing.T) {
+	assert := assert.New(t)
+
+	s, err := New(ic)
+	assert.NotNil(s)
+	assert.NoError(err)
+
+	sx, err := s.Smooth(ex[0:1], ex, mx)
+	assert.Nil(sx)
+	assert.Error(err)
+
+	sx, err = s.Smooth(ex, ex, mx)
+	assert.NotNil(sx)
 	assert.NoError(err)
 }
